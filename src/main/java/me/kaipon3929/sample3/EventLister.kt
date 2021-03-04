db634c20cc07e1e83726ea162e32f2221dc6f088
kaipon <kaipon0802@gmail.com>
2021-03-04 13:20:37 +0900

    Initial commit

---
+++ src/main/java/me/kaipon3929/sample3/EventLister.kt
@@ -0,0 +1,19 @@
+package me.kaipon3929.sample3
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
+
+object EventLister: Listener {
+    @EventHandler
+    fun on(e: PlayerJoinEvent) {
+        val p = e.player
+        e.joinMessage = "${p.displayName}がサーバーに参加しました！"
+    }
+    @EventHandler
+    fun on(e:PlayerQuitEvent) {
+        val p = e.player
+        e.quitMessage = "${p.displayName}がサーバーから退出しました！"
+    }
+}
---
+++ src/main/java/me/kaipon3929/sample3/Main.kt
@@ -0,0 +1,14 @@
+package me.kaipon3929.sample3
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main: JavaPlugin() {
+    override fun onEnable() {
+        logger.info("プラグインが起動したよ")
+        server.pluginManager.registerEvents(EventLister, this)
+    }
+
+    override fun onDisable() {
+        logger.info("プラグインが停止したよ")
+    }
+}
